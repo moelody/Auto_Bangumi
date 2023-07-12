5440746cc7e5a25e4c69e19e1dee8eb9a4624bfa
moelody <yfsmallmoon@gmail.com>
2023-07-12 11:36:49 +0800

    update html

--- backend/src/templates/assets/calendar-7aa0940f.js
+++
@@ -1,1 +0,0 @@
-import{d as e,o as n,c as a}from"./index-24be8593.js";const l=e({__name:"calendar",setup(o){return(t,c)=>(n(),a("div",null,"null"))}});export{l as default};
--- backend/src/templates/assets/downloader-641cffc9.js
+++
@@ -1,1 +0,0 @@
-import{d as e,o as n,c as o}from"./index-24be8593.js";const l=e({__name:"downloader",setup(t){return(a,r)=>(n(),o("div",null,"null"))}});export{l as default};
